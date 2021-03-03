3fd8103ea20ac388b4c25c7e2aeb08b692bf7942
d7p4n4 <Szappan60>
2021-03-03 10:26:30 +0100

    1.20210303.7

--- sources/Controllers/MicroChartInTile.controller.js
+++ sources/Controllers/MicroChartInTile.controller.js
@@ -2,15 +2,15 @@ sap.ui
     .controller(
         "sources.Controllers.MicroChartInTile",
         {
-/*
+
             onInit : function () {
                 var oMydata = new sap.ui.model.json.JSONModel();
-                oMydata.loadData("sources/Data/data.json");
+                oMydata.loadData("sources/Data/MicroChart.json");
                 console.log(JSON.stringify(oMydata.getData()));
 
                 this.getView().setModel(oMydata);
             },
-
+/*
             onTilePress : function(evt) {
                 app.to("idTile", "slide");},*/
         });
